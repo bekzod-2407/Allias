e63cb9e33ed0032dc621705d20b1e11dbba45e1f
Bekzod <bekzod.k2407@gmail.com>
2022-05-03 10:25:21 +0500

    Initial Commit

---
+++ Allias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Allias
+//
+//  Created by user on 03/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
