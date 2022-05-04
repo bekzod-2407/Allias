38f2e3c326824d6ba7933b888da1c78db22f62b4
silencepeaceoff <silencepeaceoff@gmail.com>
2022-05-04 20:25:34 +0300

    Update GameInfoViewController.swift

--- Allias/Controllers/GameInfoViewController.swift
+++ Allias/Controllers/GameInfoViewController.swift
@@ -1,9 +1,3 @@
-//
-//  GameInfoViewController.swift
-//  Allias
-//
-//  Created by user on 03/05/22.
-//
 
 import UIKit
 
@@ -12,18 +6,6 @@ class GameInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
